ef1473965b3de85649a9cce5f5bde20b37d05aba
steven midgley <steven.midgley@maggiesottero.com>
2024-05-05 17:11:17 -0600

    Added a very basic picture component.

--- src/app/utilities/image/image.component.ts
+++ src/app/utilities/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './image.component.scss'
 })
 export class ImageComponent {
-
+  @Input() imageSrc!: string;
+  @Input() imageAlt!: string;
 }
