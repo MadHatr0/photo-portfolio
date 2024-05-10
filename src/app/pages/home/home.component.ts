a12e08319cdef5980feb6f03e6bc4b69e2fef866
steven midgley <steven.midgley@maggiesottero.com>
2024-05-10 00:07:39 -0600

    Hell ya! got it to work. Mostly. It just isnt consistent yet.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,32 +9,30 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class HomeComponent implements AfterViewInit {
   @ViewChild('homeImageContainer') container!: ElementRef;
-  @ViewChild('magikCursor') magikCursor!: ElementRef;
 
   ngAfterViewInit(): void {
     const imageContainer = this.container.nativeElement;
-    const magikCursor = this.container.nativeElement;
-
 
     imageContainer.addEventListener('mousemove', (e: MouseEvent) => {
-      const radius = 50; // half of the radius. This puts the cursor in the center
-      const rect = imageContainer.getBoundingClientRect(); // get bounds of image container
-      const mX = e.clientX - rect.left - radius; // set cursor position
-      const mY = e.clientY - rect.top - radius; // set cursor position
-      console.log(`mX: ${mX}, mY: ${mY}`)
+      const radius = 100; // half of the radius. This puts the cursor in the center
+      const rect = imageContainer.getBoundingClientRect();
+      const mX = e.clientX - rect.left - radius;
+      const mY = e.clientY - rect.top - radius;
+      const underImage = imageContainer.querySelector('.under-image') as HTMLElement;
+      const elementsAtCursor: any[] = document.elementsFromPoint(e.clientX, e.clientY);
+
       imageContainer.style.setProperty('--cursor-x', `${mX}px`)
       imageContainer.style.setProperty('--cursor-y', `${mY}px`)
 
-      const underImage = imageContainer.querySelector('.under-image') as HTMLElement;
-      underImage.style.opacity = '0'
+      if (elementsAtCursor.includes(underImage)) {
+        const underImageRec = underImage.getBoundingClientRect();
+        const relX = (e.clientX - underImageRec.left) / underImageRec.width;
+        const relY = (e.clientY - underImageRec.top) / underImageRec.height;
 
-      // // allows the magik-cursor to follow mouse movement
-      // magikCursor.style.left = `${mX}px`
-      // magikCursor.style.top = `${mY}px`
-
-      const doMagik: any = document.elementsFromPoint(e.clientX, e.clientY);
-      if (doMagik.some((e: any) => e === underImage)) {
-        underImage.style.opacity = '1';
+        underImage.style.clipPath = `circle(100px at ${relX * 100}% ${relY * 100}%)`
+        underImage.style.opacity = '1'
+      } else {
+        underImage.style.opacity = '0'
       }
     })
   }
