88ecd47ee582ca575bf88e57e036fb66237a1166
steven midgley <steven.midgley@maggiesottero.com>
2024-05-09 21:13:35 -0600

    Getting started on homepage.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'albums', component: AlbumsComponent }
+];
